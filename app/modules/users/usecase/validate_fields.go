5acc5896e4990b13c3812d96e7bbeb2e2f3c0c29
Nha-Voyager <nhalt@vayager-hcm.com>
2023-08-27 23:53:56 +0700

    fix load config and errors messages

--- app/modules/users/usecase/validate_fields.go
+++ app/modules/users/usecase/validate_fields.go
@@ -1,6 +1,8 @@
 package usecase
 
 import (
+	"reflect"
+
 	"github.com/go-playground/validator/v10"
 )
 
@@ -9,22 +11,25 @@ func (u UserUseCase) ValidateFields(payload interface{}) ([]map[string]string, e
 	err := validate.Struct(payload)
 	if err != nil {
 		errorMessages := []map[string]string{}
+		payloadType := reflect.TypeOf(payload).Elem()
 		for _, fieldError := range err.(validator.ValidationErrors) {
 			fieldName := fieldError.Field()
+			field, _ := payloadType.FieldByName(fieldName)
+			jsonTag := field.Tag.Get("json")
 			errorMessage := ""
 			switch fieldError.Tag() {
 			case "required":
-				errorMessage = fieldName + " is required"
+				errorMessage = jsonTag + " is required"
 			case "min":
-				errorMessage = fieldName + " must have a minimum length of " + fieldError.Param()
+				errorMessage = jsonTag + " must have a minimum length of " + fieldError.Param()
 			case "max":
-				errorMessage = fieldName + " must have a maximum length of " + fieldError.Param()
+				errorMessage = jsonTag + " must have a maximum length of " + fieldError.Param()
 			default:
 				errorMessage = fieldError.Error()
 			}
 
 			errorMessages = append(errorMessages, map[string]string{
-				fieldName: errorMessage,
+				jsonTag: errorMessage,
 			})
 		}
 
