35565792528855e9f6a2d8a17bd1b57f354a5ee0
VoyagerLieu <lieunguyen.voyager@gmail.com>
2023-09-07 09:52:14 +0700

    refactor

--- app/modules/book_categories/repositories/get_by_id.go
+++ app/modules/book_categories/repositories/get_by_id.go
@@ -5,7 +5,7 @@ import "github.com/team2/real_api/app/models"
 func (r BookCategoryRepo) GetByID(id int) (*models.BookCategory, error) {
 	var bookCategory *models.BookCategory
 
-	result := r.DB.Table(models.BookCategory{}.TableName()).Preload("User").Preload("Books").Where("id = ?", id).First(&bookCategory)
+	result := r.DB.Table(models.BookCategory{}.TableName()).Preload("User").Preload("Books").Where("id = ? AND deleted_at IS NULL", id).First(&bookCategory)
 
 	if result.Error != nil {
 		return nil, result.Error
--- app/modules/book_categories/repositories/get_list.go
+++ app/modules/book_categories/repositories/get_list.go
@@ -5,7 +5,7 @@ import "github.com/team2/real_api/app/models"
 func (r BookCategoryRepo) GetList() ([]*models.BookCategory, error) {
 	var bookCategories []*models.BookCategory
 
-	result := r.DB.Table(models.Book{}.TableName()).Preload("User").Preload("Books").Find(&bookCategories)
+	result := r.DB.Table(models.BookCategory{}.TableName()).Preload("User").Preload("Books").Find(&bookCategories)
 
 	if result.Error != nil {
 		return nil, result.Error
